59b22aacf065eacf6da8f63369158a8713db7e24
Sabrinaaccardo <saccardo22@fed.idserve.net>
2022-11-16 14:44:38 +0100

    Create xcode file

---
+++ NC1 Voice memos SA/NC1 Voice memos SA/NC1_Voice_memos_SAApp.swift
@@ -0,0 +1,16 @@
+//
+//  NC1_Voice_memos_SAApp.swift
+//  NC1 Voice memos SA
+//  Created by Sabrina Accardo on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NC1_Voice_memos_SAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
